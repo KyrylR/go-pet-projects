86fdd0596894cf8cd8a3dc701c1802c4ff40b375
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-10 18:55:29 +0300

    Add pxcanvas and pxcanvasrenderer to app

--- pixl/pixl/main.go
+++ pixl/pixl/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"image/color"
 	"pixl/apptype"
+	"pixl/pxcanvas"
 	"pixl/swatch"
 	"pixl/ui"
 )
@@ -17,7 +19,18 @@ func main() {
 		SwatchSelected: 0,
 	}
 
+	pixlCanvasConfig := apptype.PxCanvasConfig{
+		Drawing:      fyne.NewSize(600, 600),
+		CanvasOffset: fyne.NewPos(0, 0),
+		PxRows:       10,
+		PxCols:       10,
+		PxSize:       30,
+	}
+
+	pixlCanvas := pxcanvas.NewPxCanvas(&state, pixlCanvasConfig)
+
 	appInit := ui.AppInit{
+		PixlCanvas: pixlCanvas,
 		PixlWindow: pixlWindow,
 		State:      &state,
 		Swatches:   make([]*swatch.Swatch, 0, 64),
