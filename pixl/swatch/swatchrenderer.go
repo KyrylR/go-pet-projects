f394feb7013cd8c1f2264958a2ef18e85baa5009
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-10 16:47:41 +0300

    add Swatch struct with implemented interfaces

---
+++ pixl/swatch/swatchrenderer.go
@@ -0,0 +1,41 @@
+package swatch
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"image/color"
+)
+
+type SwatchRenderer struct {
+	square  canvas.Rectangle
+	objects []fyne.CanvasObject
+	parent  *Swatch
+}
+
+func (s *SwatchRenderer) Destroy() {}
+
+func (s *SwatchRenderer) Layout(size fyne.Size) {
+	s.objects[0].Resize(size)
+}
+
+func (s *SwatchRenderer) MinSize() fyne.Size {
+	return s.square.MinSize()
+}
+
+func (s *SwatchRenderer) Objects() []fyne.CanvasObject {
+	return s.objects
+}
+
+func (s *SwatchRenderer) Refresh() {
+	s.Layout(fyne.NewSize(20, 20))
+	s.square.FillColor = s.parent.Color
+	if s.parent.Selected {
+		s.square.StrokeWidth = 3
+		s.square.StrokeColor = color.RGBA{R: 255, G: 255, B: 255, A: 255}
+		s.objects[0] = &s.square
+	} else {
+		s.square.StrokeWidth = 0
+		s.objects[0] = &s.square
+	}
+	canvas.Refresh(s.parent)
+}
